a7e94640653cd9a39b640db467eb118f4f6f29c2
hase62 <hase62@users.noreply.github.com>
2020-04-23 18:01:56 +0900

    Minor Update

--- R/TestAnalysis.R
+++ R/TestAnalysis.R
@@ -5,6 +5,7 @@
 #'@export
 TestAnalysis<-function(){
   data("sample_vcf.annovar")
+  data("sample_vcf.vep")
   data("sample_hla_table_c1")
   data("sample_refFlat.grch37")
   data("sample_refMrna.grch37.fa")
@@ -15,6 +16,13 @@ TestAnalysis<-function(){
                 refflat_file = sample_refFlat.grch37,
                 refmrna_file = sample_refMrna.grch37.fa,
                 netMHCpan_dir = NA)
+
+  MainSNVClass1(input_vep_format_file = sample_vcf.vep,
+                hla_types = sample_hla_table_c1[1,-1],
+                refflat_file = sample_refFlat.grch37,
+                refmrna_file = sample_refMrna.grch37.fa,
+                netMHCpan_dir = NA)
+
 
   write.table(file = "result.ID.SNV1/data.ID_SNV.peptide.SNV_CLASS1_ALL.txt",
               x = sample_result_SNV_CLASS1_ALL[grep("0_DHX15", sample_result_SNV_CLASS1_ALL$Gene), ],
